d8f67139bc6418849dd028903f43b7b69d59d0c7
David Huh <davidmhuh@gmail.com>
2018-10-08 10:13:54 -0700

    Law

--- models/workout.js
+++ models/workout.js
@@ -1,10 +1,17 @@
-module.exports = function(sequelize, DataTypes) {
-    var Workout = sequelize.define("Workout", {
-      category: DataTypes.STRING,
-      distance: DataTypes.INTEGER, 
-      workout_length: DataTypes.TIME,
-      RPE: DataTypes.INTEGER, 
-      stress_score: DataTypes.INTEGER
-    });
-    return Workout;
-  };
+module.exports = function (sequelize, DataTypes) {
+  var Workout = sequelize.define("Workout", {
+    category: DataTypes.STRING,
+    distance: DataTypes.INTEGER,
+    workout_length: DataTypes.TIME,
+    RPE: DataTypes.INTEGER,
+    stress_score: DataTypes.INTEGER,
+    strava_id: DataTypes.INTEGER,
+    date: DataTypes.DATEONLY,
+    time: DataTypes.TIME,
+    has_heartrate: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false
+    }
+  });
+  return Workout;
+};
