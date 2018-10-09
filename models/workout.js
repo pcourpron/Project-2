cce495fc9f281dde9da3b26a923f3eb68d64271d
pcourpron <pierrecourpron@gmail.com>
2018-10-09 11:11:21 -0700

    merge

--- models/workout.js
+++ models/workout.js
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
-
-module.exports = function(sequelize, DataTypes) {
-
-  var Workout = sequelize.define('Workout', {
-    user_id: DataTypes.STRING,
-=======
 module.exports = function (sequelize, DataTypes) {
   var Workout = sequelize.define("Workout", {
->>>>>>> 53fd7893662feb742a36678d84e2b199443d4832
     category: DataTypes.STRING,
     distance: DataTypes.INTEGER,
     workout_length: DataTypes.TIME,
