b4d9f3a8ac9930b3fb54e93d50e4e9585b27f298
Pierre <pierrecourpron@gmail.com>
2019-05-19 19:45:48 +0200

    hrv

--- routes/hrv.js
+++ routes/hrv.js
@@ -7,10 +7,10 @@ app.post("/dalembert", callD_alembert);
 
 
 function callD_alembert(req, res) {
-  res.send('hi')
   PythonShell.run("./python/hrv.py", null, function (err, data) {
     console.log('==========')
     console.log('results: %j', data);
+    res.send(data)
     let results = JSON.parse(data[0].replace(/'/g,"\""));
     console.log(results.test)
     console.log('==========')
